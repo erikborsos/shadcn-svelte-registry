943c350d947a9b80a2375e3055dd359d3559f745
erikborsos <erikborsos21@gmail.com>
2026-08-17 22:08:35 +0200

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,29 @@
+import tailwindcss from "@tailwindcss/vite"
+import adapter from "@sveltejs/adapter-static"
+import { sveltekit } from "@sveltejs/kit/vite"
+import { defineConfig } from "vite"
+
+const dev = process.env.NODE_ENV === "development"
+
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit({
+			compilerOptions: {
+				runes: ({ filename }) =>
+					filename.split(/[/\\]/).includes("node_modules") ? undefined : true
+			},
+
+			adapter: adapter({
+				pages: "build",
+				assets: "build",
+				fallback: "index.html",
+				precompress: false,
+				strict: true
+			}),
+			paths: {
+				base: dev ? "" : "/shadcn-svelte-registry"
+			}
+		})
+	]
+})
